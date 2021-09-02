cf3652b18aa02b75687df9b54b7f77b0f688d11e
paulinemarg <paulinemargossian@gmail.com>
2021-09-02 21:52:52 +0200

    Create NumberofEvents.test.js

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import NumberOfEvents from '../NumberOfEvents';
+
+describe('<NumberOfEvents /> component', () => {
+  let NumberOfEventsWrapper;
+  beforeAll(() => {
+    NumberOfEventsWrapper = shallow(<NumberOfEvents updateEvents={() => { }} />);
+  });
+  //
+  test('render text input', () => {
+    expect(NumberOfEventsWrapper.find('.numberOfEvents')).toHaveLength(1);
+  });
+  //
+  test('renders text input correctly', () => {
+    const numberOfEvents = NumberOfEventsWrapper.state('numberOfEvents');
+    expect(
+      NumberOfEventsWrapper.find('#numberOfEvents__input').prop('value')
+    ).toBe(numberOfEvents);
+  });
+  //
+  test('change state when input changes', () => {
+    const eventObject = { target: { value: 32 } };
+    NumberOfEventsWrapper.find('#numberOfEvents__input').simulate(
+      'change',
+      eventObject
+    );
+    expect(NumberOfEventsWrapper.state('numberOfEvents')).toBe(32);
+  });
+});
