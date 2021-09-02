57f96be7f610cb7249f50ddc68c8b8e478d2b963
paulinemarg <paulinemargossian@gmail.com>
2021-09-02 21:52:23 +0200

    Create Event.js

---
+++ src/Event.js
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class Event extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //event: {},
+      expanded: false,
+    };
+    this.toggleExpanded = this.toggleExpanded.bind(this);
+  }
+  toggleExpanded() {
+    this.setState({
+      expanded: !this.state.expanded,
+    });
+  }
+  render() {
+    let event = this.props.event;
+    return (
+      <div className="event">
+        <h2 className="event__Overview--name">{event.summary}</h2>
+        <div className="basic-info">
+          <h2 className="event-location">{event.location}</h2>
+          <h3 className="event__Overview--localDate">
+            start: {event.start.dateTime} - Time Zone: {event.start.timeZone}
+          </h3>
+          {this.state.expanded === true && (
+            <p className="event-details">{event.description}</p>
+          )}
+        </div>
+        <button className="details-btn" onClick={this.toggleExpanded}>
+          {!this.state.expanded ? 'Show Details' : 'Hide Details'}
+        </button>
+      </div>
+    );
+  }
+}
+export default Event;
