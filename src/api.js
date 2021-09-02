f86834f6a1d2718f68a477be6bf028486f657aa4
paulinemarg <paulinemargossian@gmail.com>
2021-09-02 21:50:37 +0200

    Create api.js

---
+++ src/api.js
@@ -0,0 +1,6 @@
+// This function takes an events array, then uses map to create a new array with only locations.
+export const extractLocations = (events) => {
+  var extractLocations = events.map((event) => event.location);
+  var locations = [...new Set(extractLocations)];
+  return locations;
+};
