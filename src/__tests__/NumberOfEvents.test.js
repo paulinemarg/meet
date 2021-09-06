9451f1c00b5bb3ce1f8e4d86ac19c191933b9e9d
paulinemarg <paulinemargossian@gmail.com>
2021-09-06 18:30:10 +0200

    Update NumberofEvents.test.js
    removed dash

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -7,18 +7,15 @@ describe('<NumberOfEvents /> component', () => {
   beforeAll(() => {
     NumberOfEventsWrapper = shallow(<NumberOfEvents updateEvents={() => { }} />);
   });
-  //
   test('render text input', () => {
     expect(NumberOfEventsWrapper.find('.numberOfEvents')).toHaveLength(1);
   });
-  //
   test('renders text input correctly', () => {
     const numberOfEvents = NumberOfEventsWrapper.state('numberOfEvents');
     expect(
       NumberOfEventsWrapper.find('#numberOfEvents__input').prop('value')
     ).toBe(numberOfEvents);
   });
-  //
   test('change state when input changes', () => {
     const eventObject = { target: { value: 32 } };
     NumberOfEventsWrapper.find('#numberOfEvents__input').simulate(
