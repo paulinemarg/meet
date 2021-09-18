9b08bf1625085a1dadf883d841a556edbe354dba
paulinemarg <paulinemargossian@gmail.com>
2021-09-18 22:31:52 +0200

    imported welcome screen

--- src/App.js
+++ src/App.js
@@ -3,9 +3,10 @@ import './App.css';
 import EventList from './EventList';
 import CitySearch from './CitySearch';
 import NumberOfEvents from './NumberOfEvents';
-import { getEvents, extractLocations } from './api';
+import { getEvents, extractLocations, checkToken, getAccessToken } from './api';
 import { WarningAlert } from './Alert';
 import './nprogress.css';
+import WelcomeScreen from './WelcomeScreen';
 
 class App extends Component {
   state = {
@@ -13,30 +14,39 @@ class App extends Component {
     locations: [],
     currentLocation: 'all',
     numberOfEvents: 32,
-    warningText: ""
+    warningText: "",
+    showWelcomeScreen: undefined
   }
 
-  componentDidMount() {
+  async componentDidMount() {
     this.mounted = true;
-    getEvents().then((events) => {
-      if (this.mounted) {
-        this.setState({
-          events: events.slice(0, this.state.numberOfEvents),
-          locations: extractLocations(events)
-        });
-      }
-      if (navigator.onLine) {
-        console.log('online');
-        this.setState({
-          warningText: "",
-        });
-      } else {
-        console.log('offline');
-        this.setState({
-          warningText: "You are currently using the app offline."
-        });
-      };
-    });
+    const accessToken = localStorage.getItem('access_token');
+    const isTokenValid = (await checkToken(accessToken)).error ? false :
+      true;
+    const searchParams = new URLSearchParams(window.location.search);
+    const code = searchParams.get("code");
+    this.setState({ showWelcomeScreen: !(code || isTokenValid) });
+    if ((code || isTokenValid) && this.mounted) {
+      getEvents().then((events) => {
+        if (this.mounted) {
+          this.setState({
+            events: events.slice(0, this.state.numberOfEvents),
+            locations: extractLocations(events)
+          });
+        }
+      });
+    }
+    if (navigator.onLine) {
+      console.log('online');
+      this.setState({
+        warningText: "",
+      });
+    } else {
+      console.log('offline');
+      this.setState({
+        warningText: "You are currently using the app offline."
+      });
+    };
   }
 
   componentWillUnmount() {
@@ -67,15 +77,18 @@ class App extends Component {
   }
 
   render() {
+    if (this.state.showWelcomeScreen === undefined) return <div className="App" />
     return (
       <>
         <div className="App">
           <h1>Let's Meet-App</h1>
           <WarningAlert text={this.state.warningText} />
           <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
+          <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} />
           <EventList className="eventlist" events={this.state.events} />
           <button className="scrollup-btn" onClick={this.scrollTop}>&#8593;</button>
-          <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} />
+          <WelcomeScreen showWelcomeScreen={this.state.showWelcomeScreen}
+            getAccessToken={() => { getAccessToken() }} />
         </div>
       </>
     );
