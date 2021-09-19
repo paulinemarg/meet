14ee225dfb584dcdc71db3b3362949e89f4c0ab9
paulinemarg <paulinemargossian@gmail.com>
2021-09-19 14:08:29 +0200

    added svg

--- src/App.js
+++ src/App.js
@@ -25,7 +25,13 @@ class App extends Component {
     const searchParams = new URLSearchParams(window.location.search);
     const code = searchParams.get("code");
     this.setState({ showWelcomeScreen: !(code || isTokenValid) });
-
+    /*this.setState({
+      showWelcomeScreen: !(
+        code ||
+        isTokenValid ||
+        window.location.hostname === 'localhost'
+      )
+    }); */
     if ((code || isTokenValid) && this.mounted) {
       getEvents().then((events) => {
         if (this.mounted) {
@@ -81,7 +87,10 @@ class App extends Component {
     return (
       <>
         <div className="App">
-          <h2>Let's Meet-App</h2>
+          <h2>Meet <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-geo-alt" viewBox="0 0 16 16">
+            <path d="M12.166 8.94c-.524 1.062-1.234 2.12-1.96 3.07A31.493 31.493 0 0 1 8 14.58a31.481 31.481 0 0 1-2.206-2.57c-.726-.95-1.436-2.008-1.96-3.07C3.304 7.867 3 6.862 3 6a5 5 0 0 1 10 0c0 .862-.305 1.867-.834 2.94zM8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10z" />
+            <path d="M8 8a2 2 0 1 1 0-4 2 2 0 0 1 0 4zm0 1a3 3 0 1 0 0-6 3 3 0 0 0 0 6z" />
+          </svg></h2>
           <WarningAlert text={this.state.warningText} />
           <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
           <NumberOfEvents numberOfEvents={this.state.numberOfEvents} updateEvents={this.updateEvents} />
